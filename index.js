c82853f9cbf4f085ac76573619e8e819aa2b9544
AyanaZaire <ayanazaire@gmail.com>
2021-07-20 12:49:15 -0400

    refactored app from cli to web

--- index.js
+++ index.js
@@ -1,153 +1,139 @@
-const prompt = require('prompt');
-const fetch = require("node-fetch");
+ENTRIES_URL = "http://localhost:3000/entries"
+FAVORITES_URL = "http://localhost:3000/favorites/"
+var allEntries = []
 
-let menu = {
-    1: "View Library",
-    2: "View Favorites",
-    3: "Add to Favorites",
-    4: "Edit Favorites",
-    5: "Generate Poem",
-    6: "Leave the Library"
-};
+document.addEventListener("DOMContentLoaded", () => {
+  //console.log("We live");
+  loadEntries()
+})
 
 function loadEntries() {
-  if (allEntries.length === 0) {
-    fetch("http://localhost:3000/entries")
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
-        allEntries.push(entry)
-      })
+  fetch(ENTRIES_URL)
+  .then(resp => resp.json())
+  .then(entries => {
+    //console.log(entries);
+    let entriesList = document.getElementById("entries-list")
+    //console.log(entriesList);
+    entries.forEach(entry => {
+      allEntries.push(entry)
+      // list entries in unordered list
+      let li = document.createElement("li")
+      //li.id = entry.id
+      li.innerHTML = entry.name
+      let button = document.createElement("button")
+      button.id = entry.id
+      button.className = "add-to-favorites"
+      button.innerText = "Add to Favorites"
+      li.append(button)
+      entriesList.append(li)
     })
-  } else {
-    return allEntries
-  }
-}
-
-function entryIndex() {
-  allEntries.forEach(entry => {
-    console.log(entry.id, entry.name);
+    favoriteIndex()
+    addToFavoritesHandler()
   })
-  console.log("If you wish to view a single entry and/or add it to your favorites, please input the corresponding number below.");
-  prompt.start();
-  prompt.get(['entry'], function (err, result) {
-    console.log('Command-line input received:');
-    viewEntry(result.entry)
-  });
 }
 
-function viewEntry(id) {
-  // could filter through allEntries array to eliminate this call to database?
-  fetch(`http://localhost:3000/entries/${parseInt(id)}`)
+function addToFavoritesHandler() {
+  document.addEventListener("click", () => {
+    //console.log(event.target);
+    if(event.target.className == "add-to-favorites") {
+      //console.log(event.target.id);
+      let entryId = parseInt(event.target.id)
+      addToFavorites(entryId)
+    }
+  })
+}
+
+function addToFavorites(entryId) {
+  let data = {entryId: entryId, profileId: 1}
+  //console.log(entryId);
+  fetch(FAVORITES_URL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  })
   .then(response => response.json())
-  .then(entry => {
-    console.log('\n' +
-      "Name:", entry.name + '\n' +
-      "Year Born:", entry.born + '\n' +
-      "Birthplace:", entry.birthplace + '\n' +
-      "Bio:", entry.bio + '\n');
-    console.log("Do you wish to add this entry to your Favorites? Please input yes or no below.");
-    prompt.start();
-    prompt.get(['input'], function (err, result) {
-      console.log('Yes/no input received:');
-      if (result.input == "yes") {
-        addFavorite(entry)
-      }
-      if (result.input == "no") {
-        launchLibrary()
-      }
-    });
+  .then(favorite => {
+    //console.log(favorite);
+    var favoritedEntry = allEntries.filter(entry => entry.id == favorite.entryId)
+    let favoritesDiv = document.getElementById("favorites")
+    let singleFav = document.createElement("div")
+    //console.log(favoritedEntry[0]);
+    singleFav.innerHTML = `<p>${favoritedEntry[0].name}</p><button data-id=${favoritedEntry[0].id}>Remove</button>`
+    favoritesDiv.append(singleFav)
   })
 }
 
 function favoriteIndex() {
-  fetch("http://localhost:3000/favorites")
-  .then(response => response.json())
+  fetch(FAVORITES_URL)
+  .then(resp => resp.json())
   .then(favorites => {
-    if (favorites.length === 0) {
-      console.log("No favorites added yet. To add a favorite view entries below.");
-      entryIndex()
-    } else {
-      for (var i = 0; i < favorites.length; i++) {
-        favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
-        favoritedEntries.forEach(entry => {
-          console.log('\n' +
-            "Entry ID:", entry.id + '\n' +
-            "Name:", entry.name + '\n' +
-            "Year Born:", entry.born + '\n' +
-            "Birthplace:", entry.birthplace + '\n' +
-            "Bio:", entry.bio + '\n');
-        })
-      }
-      removeFavorite(favorites)
+    //console.log(favorites);
+    //console.log(allEntries);
+    for (var i = 0; i < favorites.length; i++) {
+      var favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
+      let favoritesDiv = document.getElementById("favorites")
+      favoritedEntries.forEach(entry => {
+        //console.log(favorites[i].id);
+        let singleFav = document.createElement("div")
+        singleFav.id = favorites[i].id
+        singleFav.className ="favorite-div"
+        singleFav.innerHTML = `<p>${entry.name}</p><button class="remove-button" data-id=${favorites[i].id}>Remove</button>`
+        favoritesDiv.append(singleFav)
+      })
+    }
+    removeFavoriteHandler(favorites)
+    splitFavoriteBios()
+  })
+}
+
+function removeFavoriteHandler(favorites) {
+  document.addEventListener("click", () => {
+    if (event.target.className == "remove-button") {
+      removeFavorite(favorites, event.target, event.target.dataset.id);
     }
   })
 }
 
-function removeFavorite(favorites) {
-  console.log("If you would like to remove an entry from your favorites, please input the corresponding Entry ID below. If you would like to return to main menu type, menu");
-  prompt.start();
-  prompt.get(['entry'], function (err, result) {
-    console.log('Command-line input received:', result.entry);
-    if (result.entry == "menu") {
-      launchLibrary()
-    } else {
-      let removedFavorites = favorites.filter(favorite => favorite.entryId == result.entry)
-      let removedFavorite = removedFavorites[0]
-      fetch(`http://localhost:3000/favorites/${removedFavorite.id}`, {
-        method: "DELETE",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(removedFavorite)
-      })
-      .then(response => response.json())
-      .then(deletedFavorite => {
-        console.log("Entry removed from favorites successfully! View current favorites below:");
-        favoriteIndex()
-      })
-    }
-  });
-}
-
-function addFavorite(entry) {
-  let favorite = {
-    entryId: entry.id,
-    profileId: 1
-  }
-  fetch("http://localhost:3000/favorites", {
-    method: "POST",
+function removeFavorite(favorites, button, id) {
+  let favoriteToRemove = favorites.filter(fav => fav.id == id)
+  let data = favoriteToRemove[0]
+  fetch(FAVORITES_URL + `${id}`, {
+    method: "DELETE",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(favorite)
+    body: JSON.stringify(data)
   })
-  .then(response => response.json())
+  .then(resp => resp.json())
   .then(favorite => {
-    console.log("Favorite added:", entry.name)
-    launchLibrary()
+    let divToRemove = button.parentElement
+    divToRemove.innerHTML = ""
   })
 }
 
-// IDEA: Generates a poem using the bio's of your favorited entries
 function splitFavoriteBios() {
-  fetch("http://localhost:3000/favorites")
-  .then(response => response.json())
-  .then(favorites => {
-    if (favorites.length === 0) {
-      console.log("No favorites added yet. To add a favorite view entries below.");
-      entryIndex()
-    } else {
-      var singleArrayOfSentences = []
-      for (var i = 0; i < favorites.length; i++) {
-        favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
-        favoritedEntries.forEach(entry => {
-          console.log(entry.name, entry.id);
-          let splitBio = entry.bio.split(".")
-          // push each sentence into a single array
-          for (let i = 0, len = splitBio.length; i < len ; i++) {
-            singleArrayOfSentences.push(splitBio[i])
-          }
-        })
+  let poemButton = document.getElementById("generate-poem")
+  poemButton.addEventListener("click", () => {
+    fetch("http://localhost:3000/favorites")
+    .then(response => response.json())
+    .then(favorites => {
+      if (favorites.length === 0) {
+        console.log("No favorites added yet. To add a favorite view entries below.");
+        entryIndex()
+      } else {
+        var singleArrayOfSentences = []
+        for (var i = 0; i < favorites.length; i++) {
+          favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
+          favoritedEntries.forEach(entry => {
+            console.log(entry.name, entry.id);
+            let splitBio = entry.bio.split(".")
+            // push each sentence into a single array
+            for (let i = 0, len = splitBio.length; i < len ; i++) {
+              singleArrayOfSentences.push(splitBio[i])
+            }
+          })
+        }
+        generatePoem(singleArrayOfSentences)
       }
-      generatePoem(singleArrayOfSentences)
-    }
+    })
   })
 }
 
@@ -170,53 +156,10 @@ function generatePoem(array) {
   }
 
   //log non-linear narrative
-  console.log(array);
+  renderPoem(array)
 }
 
-function launchLibrary() {
-  console.log("Welcome to the learning library!");
-  console.log("Input a number for one of the following menu options");
-  // create a printMenu() function instead?
-  console.log(menu);
-  // start the prompt
-  prompt.start()
-  // load the allEntries array (global variable)
-  loadEntries()
-  // display the main menu, asking the participant what they would like to do
-  prompt.get(['input'], function (err, result) {
-    console.log('Command-line input received.');
-    // View Library
-    if (result.input === "1") {
-      console.log('You chose: ' + menu[1]);
-      entryIndex()
-    }
-    // View Favorites
-    if (result.input === "2") {
-      console.log('You chose: ' + menu[2]);
-      favoriteIndex()
-    }
-    // Add to Favorites
-    if (result.input === "3") {
-      console.log('You chose: ' + menu[3]);
-      entryIndex()
-    }
-    // Edit Favorites
-    if (result.input === "4") {
-      console.log('You chose: ' + menu[4]);
-      favoriteIndex()
-    }
-    // Generate Poem
-    if (result.input === "5") {
-      console.log('You chose: ' + menu[5]);
-      splitFavoriteBios()
-    }
-    // Leave the Library
-    if (result.input === "6") {
-      console.log('You chose: ' + menu[6]);
-      console.log("You have left the library. Thank you for visiting! What have you learned? Returning to the work of Black Women Care Workers is a practice of reverence and remembrance. An act of ceromony that reminds us any freedom, joy, or love you experience within the American project is a direct result of the care of Black women.")
-    }
-  })
+function renderPoem(array) {
+  let poemDiv = document.getElementById("poem-div")
+  poemDiv.innerText = array
 }
-
-var allEntries = []
-launchLibrary()
