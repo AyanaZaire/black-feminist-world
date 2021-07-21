d582551819e726c567b19b977be5617fbfad88c2
AyanaZaire <ayanazaire@gmail.com>
2021-07-21 12:28:13 -0400

    add/remove favorites feature implemented

--- index.js
+++ index.js
@@ -29,17 +29,18 @@ function loadEntries() {
             <p class="card-text">${trimmedBio}...</p>
           </div>
           <div class="card-footer">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
-                <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"/>
-              </svg>
+
+            <div class="row">
+             <div class="col-md-3">
+               <i class="bi bi-plus-circle" data-class="add-to-favorites" id=${entry.id}></i>
+             </div>
+             <div class="col-md-3 ms-auto">
+               <i class="bi bi-arrows-fullscreen"></i>
+             </div>
+           </div>
           </div>
-        </div>
+          </div>
+
       </div>`
 
       entriesList.innerHTML += card
@@ -51,8 +52,7 @@ function loadEntries() {
 
 function addToFavoritesHandler() {
   document.addEventListener("click", () => {
-    //console.log(event.target);
-    if(event.target.className == "add-to-favorites") {
+    if(event.target.dataset.class == "add-to-favorites") {
       //console.log(event.target.id);
       let entryId = parseInt(event.target.id)
       addToFavorites(entryId)
@@ -62,7 +62,6 @@ function addToFavoritesHandler() {
 
 function addToFavorites(entryId) {
   let data = {entryId: entryId, profileId: 1}
-  //console.log(entryId);
   fetch(FAVORITES_URL, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
@@ -70,13 +69,9 @@ function addToFavorites(entryId) {
   })
   .then(response => response.json())
   .then(favorite => {
-    //console.log(favorite);
     var favoritedEntry = allEntries.filter(entry => entry.id == favorite.entryId)
     let favoritesDiv = document.getElementById("favorites")
-    let singleFav = document.createElement("div")
-    //console.log(favoritedEntry[0]);
-    singleFav.innerHTML = `<p>${favoritedEntry[0].name}</p><button data-id=${favoritedEntry[0].id}>Remove</button>`
-    favoritesDiv.append(singleFav)
+    favoritesDiv.innerHTML += `<button type="button" class="btn btn-outline-light" data-class="remove-button" id=${favorite.id}><i class="bi bi-x-circle"></i> ${favoritedEntry[0].name}</button>  `
   })
 }
 
@@ -84,29 +79,26 @@ function favoriteIndex() {
   fetch(FAVORITES_URL)
   .then(resp => resp.json())
   .then(favorites => {
-    //console.log(favorites);
-    //console.log(allEntries);
     for (var i = 0; i < favorites.length; i++) {
       var favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
       let favoritesDiv = document.getElementById("favorites")
       favoritedEntries.forEach(entry => {
-        //console.log(favorites[i].id);
-        let singleFav = document.createElement("div")
-        singleFav.id = favorites[i].id
-        singleFav.className ="favorite-div"
-        singleFav.innerHTML = `<p>${entry.name}</p><button class="remove-button" data-id=${favorites[i].id}>Remove</button>`
-        favoritesDiv.append(singleFav)
+        console.log(entry.id, favorites[i].id);
+        favoritesDiv.innerHTML += `<button type="button" class="btn btn-outline-light" data-class="remove-button" id=${favorites[i].id}><i class="bi bi-x-circle"></i> ${entry.name}</button>  `
       })
     }
     removeFavoriteHandler(favorites)
     splitFavoriteBios()
+    showEntryHandler()
   })
 }
 
+
+
 function removeFavoriteHandler(favorites) {
   document.addEventListener("click", () => {
-    if (event.target.className == "remove-button") {
-      removeFavorite(favorites, event.target, event.target.dataset.id);
+    if (event.target.dataset.class == "remove-button") {
+      removeFavorite(favorites, event.target, event.target.id);
     }
   })
 }
@@ -119,10 +111,9 @@ function removeFavorite(favorites, button, id) {
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(data)
   })
-  .then(resp => resp.json())
+  .then(response => response.json())
   .then(favorite => {
-    let divToRemove = button.parentElement
-    divToRemove.innerHTML = ""
+    button.parentNode.removeChild(button)
   })
 }
 
