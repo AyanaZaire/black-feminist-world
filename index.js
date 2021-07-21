403f6c4a1e39a999e8e2a7d6a6c8083925c94eb1
AyanaZaire <ayanazaire@gmail.com>
2021-07-21 09:21:51 -0400

    implemented bootstrap card components, split screen view, and sticky footer and navbars

--- index.js
+++ index.js
@@ -12,20 +12,37 @@ function loadEntries() {
   .then(resp => resp.json())
   .then(entries => {
     //console.log(entries);
-    let entriesList = document.getElementById("entries-list")
+    let entriesList = document.getElementById("index-panel")
     //console.log(entriesList);
     entries.forEach(entry => {
       allEntries.push(entry)
-      // list entries in unordered list
-      let li = document.createElement("li")
-      //li.id = entry.id
-      li.innerHTML = entry.name
-      let button = document.createElement("button")
-      button.id = entry.id
-      button.className = "add-to-favorites"
-      button.innerText = "Add to Favorites"
-      li.append(button)
-      entriesList.append(li)
+      let entryBioToTrim = entry.bio
+      let maxLength = 20
+      let trimmedBio = entryBioToTrim.substr(0, maxLength)
+      let card =
+      `<div class="col">
+        <div class="card h-100">
+          <img src="${entry.image}" class="card-img-top" alt="${entry.name}">
+          <div class="card-body">
+            <h5 class="card-title">${entry.name}</h5>
+            <p class="card-text"><small class="text-muted">${entry.born}</small></p>
+            <p class="card-text">${trimmedBio}...</p>
+          </div>
+          <div class="card-footer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z"/>
+                <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z"/>
+              </svg>
+          </div>
+        </div>
+      </div>`
+
+      entriesList.innerHTML += card
     })
     favoriteIndex()
     addToFavoritesHandler()
